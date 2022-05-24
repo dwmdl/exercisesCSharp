d45c826642148ff5773cc77005db718c70da91e4
proph3tum <proph3tum@gmail.com>
2022-05-24 22:08:01 +0700

    программа, которая узнает у пользователя его имя и возраст, а после выводит в форму

--- ProjForms/OutputForm.cs
+++ ProjForms/OutputForm.cs
@@ -9,10 +9,10 @@ namespace ProjForms
 		{
 			// переменная принимающая имя пользователя
 			string firstName = Interaction.InputBox("What is your first name? ", "Your First Name");
-			// переменная принимающая фамилию пользователя
-			string lastName = Interaction.InputBox("What is your last name?", "Your Last Name");
-			// помещаем в переменную текст, а также имя и фамилию пользователя 
-			string txt = "Very Pleasant " + firstName + lastName + "!";
+			// переменная принимающая возраст пользователя
+			string age = Interaction.InputBox("How old are you?", "Your age");
+			// помещаем в переменную текст, а также имя и возраст пользователя 
+			string txt = "Very Pleasant " + firstName + " Your age: " + age;
 			// выводим на экран все данные и указываем название окна вторым аргументом
 			MessageBox.Show(txt, @"The acquaintance took place");
 
