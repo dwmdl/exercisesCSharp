633d4c7d22e84835f89852290d4ce509d610a6d5
proph3tum <proph3tum@gmail.com>
2022-05-24 11:12:14 +0700

    first projects C#

---
+++ ProjConsole/InputConsole.cs
@@ -0,0 +1,19 @@
+﻿namespace Proj_1;
+/*
+ Необходимо написать консольную версию программ, которая сначала принимает имя пользователя, потом фамилию и выводит эту информацию на в консоль
+*/
+
+static class InputConsole
+{
+	static void Main()
+	{
+		Console.Title = "Приветствие";
+		Console.WriteLine("Введите свое имя");
+		// объявляем строковую переменную и сразу считываем ее
+		string? firstName = Console.ReadLine();
+		Console.WriteLine("Введите свою фамилию");
+		string? secondName = Console.ReadLine();
+		// выводим на экран имя(0 индекс) и фамилию(1 индекс) пользователя
+		Console.WriteLine("Здравствуйте, {0} {1} ", firstName, secondName);
+	}
+}
---
+++ ProjForms/InputForms.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ProjForms
+{
+	static class InputForms
+	{
+		static void Main()
+		{
+			// переменная принимающая имя пользователя
+			string firstName = Interaction.InputBox("What is your first name? ", "Your First Name");
+			// переменная принимающая фамилию пользователя
+			string lastName = Interaction.InputBox("What is your last name?", "Your Last Name");
+			// помещаем в переменную текст, а также имя и фамилию пользователя 
+			string txt = "Very Pleasant " + firstName + lastName + "!";
+			// выводим на экран все данные и указываем название окна вторым аргументом
+			MessageBox.Show(txt, @"The acquaintance took place");
+
+		}
+	}
+}
