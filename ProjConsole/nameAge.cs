ba59259f0bde972e89ebd83e8aa75a363f632771
proph3tum <proph3tum@gmail.com>
2022-05-24 11:40:03 +0700

    console app Output name and age user

---
+++ ProjConsole/nameAge.cs
@@ -0,0 +1,22 @@
+﻿namespace Proj_1;
+/*
+ * Необходимо вывести на экран возраст и имя пользователя 
+ */
+
+static class InputConsole
+{
+	static void Main()
+	{
+		// заголовок окна
+		Console.Title = "Приветствие";
+		Console.WriteLine("Введите свое имя");
+		// создаем переменную и сразу считываем информацию введенную в строке выше
+		string? firstName = Console.ReadLine();
+		Console.WriteLine("Введите свой возраст");
+		// также считываем данные из строки выше 
+		string? age = Console.ReadLine();
+		// выводим в консоль
+		Console.WriteLine("Здравствуйте, {0}", firstName);
+		Console.WriteLine("Ваш возраст: " + age);
+	}
+}
