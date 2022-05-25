9dc6223417c6fa2e46f5c147110e4c77ba2d6817
proph3tum <proph3tum@gmail.com>
2022-05-25 15:18:21 +0700

    added directory

---
+++ ProjConsole/OutputConsole.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjConsole;
+
+/*
+ * 
+ */
+
+static class OutputConsole
+{
+	static void Main()
+	{
+		Console.WriteLine("Enter any first number");
+		int firstNumber = Int32.Parse(Console.ReadLine()!);
+		Console.WriteLine("Enter any second number");
+		int secondNumber = Int32.Parse(Console.ReadLine()!);
+		int sum = firstNumber + secondNumber;
+		int difference = firstNumber - secondNumber;
+		Console.WriteLine($"Sum of numbers : {sum}");
+		Console.WriteLine($"The difference of numbers : {difference}");
+	}
+}
