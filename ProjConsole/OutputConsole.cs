0945d925799e8d71f8b0c422c319b35f9dcd3337
proph3tum <proph3tum@gmail.com>
2022-07-07 15:26:26 +0700

    check test

--- ProjConsole/OutputConsole.cs
+++ ProjConsole/OutputConsole.cs
@@ -1,63 +1,42 @@
 ﻿/* DESCRIPTION:
- * Написать программу, в которой вычисляется сумма чисел
- * Числа должны удовлетворять следующим критериям : При делении числа на 5 - в остатке 2, при делении на 3 - в остатке 1
- * Кол-во чисел в сумме вводиться пользователем
- * Программа отображает числа, которые суммируются, и значение суммы
- * Необходимо использовать обработку исключений
- * Предложить версии программ с разными операторами цикла
+ * 
  */
 
 /* STEPS:
- * 1. Запросить у пользователя кол-во чисел в сумме
- * 2. Написать проверку для введенных чисел. Можно попробовать реализовать тернарный оператор.
- *	UPD:: Все это необходимо написать внутри оператора цикла
- * 3. Программа отображает число, которое суммируется (которое прошло проверку) и значение суммы
- * 4. Реализовать обработку исключений
+ * 1.  
  */
-
 
 namespace ProjConsole;
 
-static class SumCondition
+internal static class BubbleSortArrayDemo
 {
-	static void Main()
+	private static void Main()
 	{
-		try
-		{
-			int userNumber, sum = 0;
+		// symbol variable
+		char s;
 
-			// use any suitable loop
-			do
+		// initial symbol array
+		char[] symbols = { 'Q', 'Ы', 'a', 'B', 'R', 'A', 'r', 'q', 'b' };
+
+		// displaying array
+		Console.WriteLine("Array before sorting :");
+		for (var i = 0; i < symbols.Length; i++) Console.WriteLine(symbols[i]);
+
+		// sorting elements in the array
+		for (var i = 1; i < symbols.Length; i++)
+		{
+			// iterating through the elements
+			for (int j = 0; j < symbols.Length; j++)
 			{
-				// STEPS: 1
-				Console.WriteLine("Enter a any integer \nOr enter 0 to end the program");
-				userNumber = Int32.Parse(Console.ReadLine()!);
-
-				// STEPS: 2 ; ternary operator
-				string yesNo = userNumber % 5 == 2 || userNumber % 3 == 1
-					? $"{sum} + {userNumber} = {sum += userNumber}"
-					: "The number does not match the condition. It has not been added to the total.\n";
-
-				// STEPS: 2.1 ; if operator
-				/*
-				if (userNumber % 5 == 2 || userNumber % 3 == 1)
-				{
-					Console.Write($"The number {userNumber} satisfies the conditions and is added to the total." +
-					              $"\nSum of numbers equals : {sum} + {userNumber} = {sum += userNumber}\n");
-				}
-				else Console.WriteLine("The number does not match the condition. It has not been added to the total.\n");
-				*/
-
-				// STEPS: 3
-				Console.WriteLine($"{yesNo}");
-			} while (userNumber != 0);
-
-			Console.WriteLine("Program has been ended.");
+				if (symbols[j] <= symbols[j + 1]) continue;
+				s = symbols[j + 1];
+				symbols[j + 1] = symbols[j];
+				symbols[j] = s;
+			}
 		}
-		catch
-		{
-			// STEPS: 4
-			Console.WriteLine("You entered isn't a number. Program will be end");
-		}
+
+		// displaying the contents of the array
+		Console.WriteLine("Array after sorting");
+		for (int i = 0; i < symbols.Length; i++) Console.WriteLine(symbols[i]);
 	}
 }
