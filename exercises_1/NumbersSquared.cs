b7250d5eed6f5cd771ceca207042a50ec9128af8
Никита <70805747+proph3tum@users.noreply.github.com>
2022-10-28 20:00:17 +0700

    Numbers of squared
    Вычисляет сумму квадратов чисел. Представлен метод с рекурсией и без

---
+++ exercises_1/NumbersSquared.cs
@@ -0,0 +1,26 @@
+﻿namespace exercisesCSharp;
+
+// TODO: Написать программу со статическим методом, который вычисляет сумму квадратов натуральных чисел. Число N передается аргументом методу. Необходимо предложить версию с рекурсией и без рекурсии. (on 10/28/2022, 6:03pm)
+
+public class NumbersSquared
+{
+    private static int WithoutRecursion(int n)
+    {
+        var result = 0;
+        for (var i = 1; i <= n; i++) result += i * i;
+        return result;
+    }
+
+    private static int Recursive(int n)
+    {
+        if (n <= 1) return 1;
+        return n * n + Recursive(n - 1);
+    }
+
+
+    private static void Main()
+    {
+        Console.WriteLine(WithoutRecursion(5));
+        Console.WriteLine(Recursive(5));
+    }
+}
