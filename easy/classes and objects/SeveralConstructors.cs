bd69ca3e4f823b7e114b2bc06b7c2774b71e81b9
Никита <70805747+proph3tum@users.noreply.github.com>
2022-12-22 22:16:40 +0700

    Subject : classes and objects I refused the numbering of tasks, since GitHub sorts files in a very strange way, this can be seen in the folder with tasks by methods

---
+++ easy/classes and objects/SeveralConstructors.cs
@@ -0,0 +1,30 @@
+﻿// TODO-ru : Напишите программу с классом, у которого есть два целочисленных поля. В классе должны быть описаны конструкторы, позволяющие создавать объекты без передачи аргументов, с передачей одного аргумента и с передачей двух аргументов (on 12/22/2022)
+
+// TODO-en : Write a program with a class that has two integer fields. The class must contain constructors that allow you to create objects without passing arguments, passing one argument, and passing two arguments. (on 12/22/2022)
+
+namespace exercisesCSharp.easy.classes_and_objects;
+
+public class Project
+{
+    private int _firstField = 1;
+    private int _secondField = 2;
+
+    public Project() => Console.WriteLine($"Value of fields : {_firstField} and {_secondField}");
+    public Project(int n) => Console.WriteLine($"Value of fields : {_firstField = n} and {_secondField}");
+    public Project(int x, int n) => Console.WriteLine($"Value of fields : {_firstField = x} and {_secondField = n}");
+}
+
+
+class MyClass
+{
+    static void Main()
+    {
+        Console.WriteLine($"Initially the fields have values 1 and 2\n");
+        Console.WriteLine("Constructor without arguments.");
+        Project a = new Project();
+        Console.WriteLine("Constructor with one argument.");
+        Project b = new Project(438);
+        Console.WriteLine("Constructor with two arguments.");
+        Project c = new Project(707, 814);
+    }
+}
