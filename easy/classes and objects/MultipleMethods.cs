43bedf9b25a551c2d5ceb1fda65100e4dfef8eaa
Никита <70805747+proph3tum@users.noreply.github.com>
2022-12-27 01:02:41 +0700

    Subject : Classes and ObjectS
    TODO-ru :
    Напишите программу с классом, у которого есть закрытое целочисленное поле.
    Значение полю присваивается с помощью открытого метода. Методу аргументом может передаваться целое число, а также метод может вызываться без аргументов.
    Если метод вызывается без аргументов, то поле получает нулевое значение, иначе если метод вызывается с целочисленным аргументом - значение аргумента присваивается полю.
    Однако если значение аргумента превышает 100, то значением полю присваивается число 100.
    Предусмотрите в классе конструктор, который работает по тому же принципу, что и метод для присваивания значений полю.
    Также в классе должен быть метод, позволяющий проверить значение поля. (on 12/26/2022, 11:41pm)
    
    TODO-en :
    Write a program with a class that has a private integer field.
    A value is assigned to a field using a public method. An integer can be passed as an argument to the method, and the method can also be called without arguments.
    If the method is called without arguments, then the field gets a null value, otherwise, if the method is called with an integer argument, the value of the argument is assigned to the field.
    However, if the argument value is greater than 100, then the value of the field is set to 100.
    Provide a constructor in your class that works in the same way as a method for setting values to a field.
    Also, the class must have a method that allows you to check the value of the field. (on 12/26/2022, 11:41pm)

---
+++ easy/classes and objects/MultipleMethods.cs
@@ -0,0 +1,29 @@
+﻿namespace exercisesCSharp.easy.classes_and_objects;
+
+public class Entity
+{
+    private static int Number;
+    private Entity() => Number = 0;
+    private Entity(int n) => Number = n >= 100 ? 100 : n;
+    
+    public static void Method() => Number = 0;
+    public static void Method(int n) => Number = n >= 100 ? 100 : n;
+
+    // field value check
+    public static void Show() => Console.WriteLine($"Field value : {Number}");
+
+    static void Main()
+    {
+        Method();
+        Show();
+        Method(75);
+        Show();
+        Method(101);
+        Show();
+
+        Entity dummy = new Entity();
+        Show();
+        Entity dummy2 = new Entity(10);
+        Show();
+    }
+}
