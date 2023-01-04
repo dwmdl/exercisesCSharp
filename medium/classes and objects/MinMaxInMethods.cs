07fb74c0f8697cb5ad2e1fc59e39bf5ff7cfb9bf
Никита <70805747+proph3tum@users.noreply.github.com>
2023-01-04 10:00:13 +0700

    Subject: Classes & Objects TODO-ru: Написать программу с классом, в котором есть два закрытых целочисленных поля, например max and min. Значение max не может быть меньше значения min. Значения присваиваются с помощью открытого метода. Метод может  вызываться с одним или двумя целочисленными аргументами. При вызове метода, значения полям присваиваются следующим образом: сравниваются текущие значения полей и значения аргумента/аргументов переданных методу. Самое большое значение присваивается полю max, а наименьшее полю min. Также необходимо предусмотреть конструктор, который работает по тому же принципу, что и метод. В классе также должен быть метод, который отображает значения полей. (on 12/29/2022) Само задание не сложное, но я долго пытался понять его
    TODO-en:
    Write a program with a class that has two private integer fields, such as max and min. The max value cannot be less than the min value. Values are assigned using a public method. The method can be called with one or two integer arguments. When a method is called, values are assigned to the fields in the following way: the current values of the fields and the values of the argument of the arguments passed to the method are compared. The largest value is assigned to the max field, and the smallest value to the min field. You also need to provide a constructor that works on the same principle as a method. The class must also have a method that displays the values of the fields. (on 12/29/2022)
    The task itself is not difficult, but I tried to understand it for a long time

---
+++ medium/classes and objects/MinMaxInMethods.cs
@@ -0,0 +1,66 @@
+﻿namespace exercisesCSharp.medium.classes_and_objects;
+
+public class Entity
+{
+    private Random _rnd = new Random();
+    private int _max;
+    private int _min;
+    
+    // first assignment of values to fields
+    // the field _max is always greater than _min 
+    public Entity()
+    {
+        _max = _rnd.Next(150, 200);
+        _min = _rnd.Next(1, 150);
+        Console.WriteLine($"Field values after the first assignment: max - {_max}, min - {_min}");
+    }
+
+    public void Method(int x)
+    {
+        Console.WriteLine($"\nMethod with one argument. Argument values: {x}");
+
+        if (_max < x) _max = x;
+        else if (_min > x) _min = x;
+    }
+
+    public void Method(int x, int n)
+    {
+        Console.WriteLine($"\nMethod with two arguments. Argument values: {x}, {n}");
+        if (_max < x || _max < n) _max = Math.Max(x, n);
+        else if (_min > x || _min > n) _min = Math.Min(x, n);
+
+        #region One way of the solution
+
+        /*if (_max < x)
+        {
+            _max = x;
+            if (_max < n)
+            {
+                _max = n;
+            }
+        }
+        else if (_min > x)
+        {
+            _min = x;
+            if (_min > n)
+            {
+                _min = n;
+            }
+        }*/
+
+        #endregion
+    }
+
+    public void Show() => Console.WriteLine($"Max value: {_max}, Min value: {_min}");
+
+    static void Main()
+    {
+        Random rnd = new Random();
+        Entity ent = new Entity();
+
+        ent.Method(rnd.Next(1, 136));
+        ent.Show();
+        ent.Method(rnd.Next(1, 100), rnd.Next(1, 100));
+        ent.Show();
+    }
+}
