cbb1367288b6418d4d86c6eee9acd4e7278ebf3b
Никита <70805747+proph3tum@users.noreply.github.com>
2023-01-10 14:39:44 +0700

    Subject : Classes & Objects | seventh exercise todo-ru :  Напишите программу с классом, в котором есть два поля: символьное и текстовое. В классе должен быть перегруженный метод для присваивания значений полям. Если метод вызывается с символьным аргументом, то соответствующее значение присваивается символьному полю. Если метод вызывается с текстовым аргументом, то он определяет значение текстового поля. Методу аргументом также может передаваться символьный массив. Если массив состоит из одного элемента, то он определяет значение символьного поля. В противном случае (если в массиве больше одного элемента) из символов массива формируется текстовая строка и присваивается значением текстовому полю. (on 1/10/23, 2:33PM)
     todo-en:
     Write a program with a class that has two fields: character and text. The class must have an overloaded method for assigning values to fields. If the method is called with a character argument, the corresponding value is assigned to the character field. If the method is called with a text argument, then it determines the value of the text field. A character array can also be passed as an argument to the method. If the array consists of one element, then it defines the value of the character field. Otherwise, (if there is more than one element in the array), a text string is formed from the characters of the array and assigned a value to the text field. (on 1/10/23, 2:33PM)

---
+++ easy/classes and objects/AssignSymbols.cs
@@ -0,0 +1,41 @@
+﻿namespace exercisesCSharp.easy.classes_and_objects;
+
+public class AssignSymbols
+{
+    private char _symbols;
+    private string? _txt;
+
+    private void AssignValue(char s) => Console.WriteLine($"Symbols field: {_symbols = s}");
+    private void AssignValue(string txt) => Console.WriteLine($"String field: {_txt = txt}");
+
+    private void AssignValue(char[] arraySymbs)
+    {
+        if (arraySymbs.Length > 1)
+        {
+            // foreach (var t in symbs) someTxt += Convert.ToString(t);
+            // we can covert it to linq expression
+            string someTxt = arraySymbs.Aggregate("", (current, t) => current + Convert.ToString(t));
+            AssignValue(someTxt);
+        }
+        else
+        {
+            var oneSymb = ' ';
+            foreach (var z in arraySymbs) oneSymb = Convert.ToChar(z);
+            AssignValue(oneSymb);
+        }
+    }
+
+
+    static void Main()
+    {
+        var obj = new AssignSymbols();
+        char[] symbolsArray = { 'e' };
+        char[] anotherSymbolsArray = { 'M', 'o', 'r', 'n', 'i', 'g' };
+        
+        obj.AssignValue('A');
+        obj.AssignValue("Hello");
+        
+        obj.AssignValue(symbolsArray);
+        obj.AssignValue(anotherSymbolsArray);
+    }
+}
