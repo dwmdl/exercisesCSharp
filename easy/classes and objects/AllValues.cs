8aebd3690ed167c392b65bcc300dc489144a2c77
Никита <70805747+proph3tum@users.noreply.github.com>
2023-01-10 22:03:33 +0700

    Subject : Classes & Objects | ninth exercise todo-ru: Напишите программу с классом, в котором есть статические методы, которым можно передавать произвольное количество целочисленных аргументов (или целочисленный массив). Методы, на основании переданных аргументов или массива, позволяют вычислить: наибольшее значение, наименьшее значение, а также среднее значение из набора чисел. (on 1/10/23, 10:02PM)
    todo-en:
    Write a program with a class that has static methods that can be passed an arbitrary number of integer arguments (or an integer array). Methods, based on the passed arguments or an array, allow you to calculate: the largest value, the smallest value, as well as the average value from a set of numbers. (on 1/10/23, 10:02PM)

---
+++ easy/classes and objects/AllValues.cs
@@ -0,0 +1,24 @@
+﻿namespace exercisesCSharp.easy.classes_and_objects;
+
+public static class AllValues
+{
+    static void Method(params int[] numbers)
+    {
+        // all this can be done in a different way, but using LINQ expressions is faster and easier.
+        double averageValue = numbers.Sum();
+        Console.WriteLine($"\nAverage value: {averageValue / numbers.Length}");
+        
+        var largestValue = numbers.Max();
+        Console.WriteLine($"Largest value: {largestValue}");
+        
+        var lowestValue = numbers.Min();
+        Console.WriteLine($"Lowest value: {lowestValue}");
+    }
+
+    static void Main()
+    {
+        var array = new[] { 903, 64, 163, 605, 641 };
+        Method(array);
+        Method(51, 8, 369, 933, 423);
+    }
+}
