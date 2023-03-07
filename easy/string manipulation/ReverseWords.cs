323b4e473978226993811b578cf16d818efe24bf
proph3tum <proph3tum@gmail.com>
2023-03-07 15:47:46 +0700

    Subject: String Manipulation | second exercise

---
+++ easy/string manipulation/ReverseWords.cs
@@ -0,0 +1,17 @@
+﻿// TODO-RU: Написать программу, в которой есть статический метод, возвращающий результатом текстовое значение и получающий аргументом текст. Результат метода - переданный аргументом текст, в котором слова следуют в обратном порядке. Словами считать блоки текста разделенные пробелами.
+
+// TODO-EN: Write a program that has a static method that returns a text value as a result and receives text as an argument. The result of the method is the text passed as an argument, in which the words follow in reverse order. Words are blocks of text separated by spaces.
+
+namespace exercisesCSharp;
+
+public static class ReverseWords
+{
+	private static void Reverse(string txt)
+	{
+		string[] reverseTxt = txt.Split(' ');
+		for (var i = 1; i < reverseTxt.Length + 1; i++) Console.WriteLine(reverseTxt[^i]);
+	}
+	
+	
+	private static void Main() => Reverse("How it is going?");
+}
