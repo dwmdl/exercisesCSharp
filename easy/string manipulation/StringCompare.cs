e14de5568774786efc109689fe0b27c3f2efa02d
proph3tum <proph3tum@gmail.com>
2023-03-07 17:45:44 +0700

    Subject: String Manipulation | third exercise RU - Напишите программу со статическим методом для сравнения текстовых строк. Строки на предмет совпадения сравниваются посимвольно. Правило сравнения символов такое: два символа считаются одинаковыми, если их коды отличаются не больше, чем на единицу. Текстовые строки совпадают, если у них совпадают символы.
    EN - Write a program with a static method to compare text strings. Strings are compared character by character for a match. The rule for comparing characters is this: two characters are considered the same if their codes differ by no more than one. Text strings match if they have the same characters.

---
+++ easy/string manipulation/StringCompare.cs
@@ -0,0 +1,28 @@
+﻿namespace exercisesCSharp;
+
+public static class StringCompare
+{
+	private static bool Compare(string? s1, string? s2)
+	{
+		if (s1 == null || s2 == null) return false;
+		if (s1.Length != s2.Length) return false;
+
+		for (var i = 0; i < s1.Length; i++)
+		{
+			int c1 = s1[i];
+			int c2 = s2[i];
+			if (Math.Abs(c1 - c2) > 1) return false;
+		}
+
+		return true; 
+	}
+
+	private static void Main()
+	{
+		var s1 = "UVWXY";
+		var s2 = "UVWXY";
+		var s3 = "XVWUY";
+		Console.WriteLine(Compare(s1, s2));
+		Console.WriteLine(Compare(s1, s3));
+	}
+}
