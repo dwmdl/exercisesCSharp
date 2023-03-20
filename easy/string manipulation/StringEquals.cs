98c6ed95055a89616d124c2f2f28ecc0f9bb6aa3
proph3tum <proph3tum@gmail.com>
2023-03-20 15:15:05 +0700

    Subject: String Manipulation | fourth exercise RU - Напишите программу со статическим методом, который выполняет сравнение текстовых строк. Текстовые строки сравниваются следующим образом: для каждого текстового значения определяется набор разных букв, входящих в текст(при этом кол-во вхождений буквы в текст значения не имеет) Текстовые строки считаются равными, если они содержат одинаковые наборы букв.
    EN - Write a program with a static method that compares text strings. Text strings are compared as follows: for each text value, a set of different letters included in the text is determined (the number of occurrences of a letter in the text does not matter). Text strings are considered equal if they contain the same sets of letters.

---
+++ easy/string manipulation/StringEquals.cs
@@ -0,0 +1,26 @@
+﻿namespace exercisesCSharp;
+
+public static class StringEquals
+{
+	private static bool AreStringEqual(string firstString, string secondString)
+	{
+		// create a set of characters for the first string
+		var firstSet = new HashSet<char>(firstString);
+
+		// create a set of characters for the second string
+		var secondSet = new HashSet<char>(secondString);
+
+		return firstSet.SetEquals(secondSet);
+	}
+
+
+	private static void Main()
+	{
+		var firstString = "Hello, World!";
+		var secondString = "World, Hello";
+
+		Console.WriteLine(AreStringEqual(firstString, secondString)
+			? "The string are equals"
+			: "The string aren't equals");
+	}
+}
